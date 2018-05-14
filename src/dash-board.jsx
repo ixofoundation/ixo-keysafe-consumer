e3be7cb807230a360dc0a97f0e850fac9927f38e
sourcecodesmith <herman@sourcecodesmith.com>
2018-05-14 13:50:59 +0200

    passing message to sign to Metamask

--- src/dash-board.jsx
+++ src/dash-board.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Launchbutton from './launch-button';
 
+const Eth = require('ethjs-query');
 var Web3 = require('web3');
+var web3 = null;
 
 var targets = {
   METAMASK: 0,
@@ -11,68 +13,58 @@ var targets = {
 export default class Dashboard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {messageBody: ''};
 
     // This binding is necessary to make `this` work in the callback
     this.handleLaunchEvent = this.handleLaunchEvent.bind(this);
+    this.handleMessageBodyChanged = this.handleMessageBodyChanged.bind(this);
 
-    this.state = {web3: 'undefined'};
-    this.initWeb3();
-  }
-
-  initWeb3() {
+    //init web3
     if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
-      const web3Provider = window.web3.currentProvider;
-      this.setState({web3: new Web3(web3Provider)});
+      web3 = new Web3(window.web3.currentProvider);
     } else {    
       console.error('No web3 provider found. Please install Metamask on your browser.');
       alert('No web3 provider found. Please install Metamask on your browser.');
     }
   }
 
+  handleMessageBodyChanged(e) {
+    this.setState({messageBody: e.target.value});
+  }
+
   handleLaunchEvent(target) {
     console.log(`***** target: ${target}`);
-    debugger;
 
-    var msg = '0x879a053d4800c6354e76c7985a865d2922c82fb5b3f4577b2fe08b998954f2e0'
-    
-    var from = this.state.web3.eth.accounts[0];
-    this.state.web3.eth.sign(from, msg, function (err, result) {
-      if (err) return console.error(err);
-      console.log('SIGNED:' + result);
+    this.getDidAsync().then(did=>{
+      const eth = new Eth(web3.currentProvider);
+
+      // var toSend = JSON.stringify(payload);
+      var msg = '0x' + new Buffer(this.state.messageBody).toString('hex');
+
+      eth.personal_sign(msg, did)
+          .then((signature) => {
+            console.log(`SUCCESS signature: ${signature}`);
+          });
+    }, error=>{
+      console.error(`ERROR error: ${error}`);
     });
+  }
 
-    // if (typeof window.web3 !== 'undefined') {
-    //   const payload = {key1: "value1", key2: "key2", key3: "key3"};
-      
-    //   // // This user has MetaMask, or another Web3 browser installed!
-    //   // var ethereumProvider = web3.currentProvider;
-
-    //   var web3 = new Web3(window.web3.currentProvider);
-    //   debugger;
-
-    //   var msg = '0x879a053d4800c6354e76c7985a865d2922c82fb5b3f4577b2fe08b998954f2e0'
-    //   var from = web3.eth.accounts[0];
-    //   web3.eth.sign(from, msg, function (err, result) {
-    //     if (err) return console.error(err);
-    //     console.log('SIGNED:' + result);
-    //   });
-
-    //   // web3.getAccounts(function (err, accounts) {
-    //   //   web3.eth.getBalance(accounts[0], function (err, balance) {
-    //   //     console.log('Your balance is ' + Web3.fromWei(balance, 'ether'));
-    //   //   });
-    //   // });
-    // }
-
-    // // let myWeb3 = new Web3(window.web3.currentProvider);
-    // window.web3.sign('{"key1": "value1"}').then(function (signature) {
-    //   console.log(`***** signature: ${signature}`);
-    // });
+  getDidAsync() {
+    return new Promise((resolve, reject)=>{
+      web3.eth.getAccounts(function (error, accounts) {
+        if (error) {
+          reject(error);
+        }
+        resolve(accounts[0]);
+      });
+    });
   }
 
   render() {
     return (
       <div>
+        <input value={this.state.messageBody} onChange={this.handleMessageBodyChanged} />
         <Launchbutton
           event={targets.METAMASK}
           title="Metamask" 
