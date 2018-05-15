4c06505be42bd88be065f2c7929e9d8fc71a37d0
sourcecodesmith <herman@sourcecodesmith.com>
2018-05-15 14:02:24 +0200

    attempt to keep two plugins around

--- src/dash-board.jsx
+++ src/dash-board.jsx
@@ -3,7 +3,8 @@ import Launchbutton from './launch-button';
 
 const Eth = require('ethjs-query');
 var Web3 = require('web3');
-var web3 = null;
+var web3Metamask = null;
+var web3Ixo = null;
 
 var targets = {
   METAMASK: 0,
@@ -19,13 +20,22 @@ export default class Dashboard extends React.Component {
     this.handleLaunchEvent = this.handleLaunchEvent.bind(this);
     this.handleMessageBodyChanged = this.handleMessageBodyChanged.bind(this);
 
-    //init web3
+    //init web3 for Metamask
     if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
-      web3 = new Web3(window.web3.currentProvider);
+      web3Metamask = new Web3(window.web3.currentProvider);
     } else {    
-      console.error('No web3 provider found. Please install Metamask on your browser.');
-      alert('No web3 provider found. Please install Metamask on your browser.');
+      console.error('No web3 provider for Metamask found. Please install Metamask in your browser.');
+      alert('No web3 provider for Metamask found. Please install Metamask in your browser.');
     }
+
+    //init web3 for IxoCM
+    if (typeof window.web3Ixo !== 'undefined' && typeof window.web3Ixo.currentProvider !== 'undefined') {
+      web3Ixo = new Web3(window.web3Ixo.currentProvider);
+    } else {    
+      console.error('No web3 provider for IXO Credential Manager found. Please install IXO Credential Manager in your browser.');
+      alert('No web3 provider for IXO Credential Manager found. Please install IXO Credential Manager in your browser.');
+    }
+    debugger;
   }
 
   handleMessageBodyChanged(e) {
@@ -43,16 +53,23 @@ export default class Dashboard extends React.Component {
           this.signWithMetamask(this.state.messageBody);
           break;
       case targets.IXO_CM:
-          alert('Going to sign with IXO CM.');
+          this.signWithIxoCM(this.state.messageBody);
           break;
       default:
         alert('Unknown action requested.');
     }    
   }
 
+  signWithIxoCM(message) {
+    this.getDidAsyncFromIxoCM().then(did=>{
+      console.log(`IxoCM -> DID: ${did}`);
+    });
+  }
+
   signWithMetamask(message) {
     this.getDidAsyncFromMetamask().then(did=>{
-      const eth = new Eth(web3.currentProvider);
+      console.log(`Metamask -> DID: ${did}`);
+      const eth = new Eth(web3Metamask.currentProvider);
 
       // var toSend = JSON.stringify(payload);
       var msg = '0x' + new Buffer(this.state.messageBody).toString('hex');
@@ -67,9 +84,20 @@ export default class Dashboard extends React.Component {
     });
   }
 
+  getDidAsyncFromIxoCM() {
+    return new Promise((resolve, reject)=>{
+      web3Ixo.eth.getAccounts(function (error, accounts) {
+        if (error) {
+          reject(error);
+        }
+        resolve(accounts[0]);
+      });
+    });
+  }
+
   getDidAsyncFromMetamask() {
     return new Promise((resolve, reject)=>{
-      web3.eth.getAccounts(function (error, accounts) {
+      web3Metamask.eth.getAccounts(function (error, accounts) {
         if (error) {
           reject(error);
         }
