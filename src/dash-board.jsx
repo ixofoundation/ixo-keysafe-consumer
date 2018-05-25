6b5f476a7a8cc6f5c97430efbf42e78ca8859daa
sourcecodesmith <herman@sourcecodesmith.com>
2018-05-25 11:05:58 +0200

    troubleshooting changes to get to the bottom of plugin popup from in page

--- src/dash-board.jsx
+++ src/dash-board.jsx
@@ -2,38 +2,45 @@ import React from 'react';
 import Launchbutton from './launch-button';
 import Web3 from 'web3';
 
-const web3Providers = {
-  web3Metamask: {id: 0, doShow: true, windowKey: "web3", extension: "Metamask", provider: null},
-  web3Ixo: {id: 1, doShow: true, windowKey: "web3Ixo", extension: "IXO Credential Manager", provider: null}
-};
-
 export default class Dashboard extends React.Component {
+  
   constructor(props) {
     super(props);
+
     this.state = {messageBody: ''};
+
+    this.blockchainProviders = {
+      web3Metamask: {id: 0, doShow: true, windowKey: "web3", extension: "Metamask", provider: null},
+      web3Ixo: {id: 1, doShow: true, windowKey: "web3Ixo", extension: "IXO Credential Manager", provider: null}
+    };
 
     // This binding is necessary to make `this` work in the callback
     this.handleExtensionLaunch = this.handleExtensionLaunch.bind(this);
     this.handleMessageBodyChanged = this.handleMessageBodyChanged.bind(this);
+    this.getEthereumAddressAsync = this.getEthereumAddressAsync.bind(this);
+    this.getIxoDidAsync = this.getIxoDidAsync.bind(this);
 
-    if (web3Providers.web3Metamask.doShow) {
-      web3Providers.web3Metamask.provider = this.getWeb3Instance(web3Providers.web3Metamask);
+    if (this.blockchainProviders.web3Metamask.doShow) {
+      this.initWeb3Provider(this.blockchainProviders.web3Metamask);
     }
-    if (web3Providers.web3Ixo.doShow) {
-      web3Providers.web3Ixo.provider = this.getWeb3Instance(web3Providers.web3Ixo);
+    if (this.blockchainProviders.web3Ixo.doShow) {
+      this.initWeb3Provider(this.blockchainProviders.web3Ixo);
     }
   }
 
-  getWeb3Instance(web3Provider) {
-    if (!window[web3Provider.windowKey]) {
-      web3Provider.doShow = false;
-      window.alert(`Please install ${web3Provider.extension} first.`);
-      return null;
-    }
-    if (!web3Provider.provider) {
-      // We don't know window.web3 version, so we use our own instance of web3
-      // with provider given by window.web3
-      return new Web3(window[web3Provider.windowKey].currentProvider);
+  initWeb3Provider(blockchainProvider) {
+    if (!window[blockchainProvider.windowKey]) {
+      blockchainProvider.doShow = false;
+      window.alert(`Please install ${blockchainProvider.extension} first.`);
+    } else {
+      if (!blockchainProvider.provider) {
+        if (blockchainProvider.id === this.blockchainProviders.web3Metamask.id) {
+          blockchainProvider.provider = new Web3(window[blockchainProvider.windowKey].currentProvider);
+        } else if (blockchainProvider.id === this.blockchainProviders.web3Ixo.id) {
+          // blockchainProvider.provider = window[blockchainProvider.windowKey].currentProvider;
+          blockchainProvider.provider = new Web3(window[blockchainProvider.windowKey].currentProvider);
+        }
+      }  
     }
   }
 
@@ -48,53 +55,52 @@ export default class Dashboard extends React.Component {
       return;
     }
     
-    const web3Provider = (providerId === web3Providers.web3Metamask.id)?web3Providers.web3Metamask:web3Providers.web3Ixo;
-    this.signMessageWithProvider(this.state.messageBody, web3Provider);
+    const blockchainProvider = (providerId === this.blockchainProviders.web3Metamask.id)?this.blockchainProviders.web3Metamask:this.blockchainProviders.web3Ixo;
+    this.signMessageWithProvider(this.state.messageBody, blockchainProvider);
   }
 
-  // var data = '0x' + new Buffer(this.state.messageBody).toString('hex');
-  // toHex(text) {
-  //   var hex = '';
-  //   for(var i=0;i<text.length;i++) { hex += ''+text.charCodeAt(i).toString(16); }
-  //   return `0x${hex}`;
-  // }
+  signMessageWithProvider(message, blockchainProvider) {
+    var getAddress = this.getEthereumAddressAsync;
+    if (blockchainProvider.id === this.blockchainProviders.web3Ixo.id) {
+      getAddress = this.getIxoDidAsync;
+    }
 
-  signMessageWithProvider(message, web3Provider) {
-    this.getDidAsyncFromProvider(web3Provider.provider).then(did=>{
-      console.log(`${web3Provider.extension} -> DID: ${did}`);
+    getAddress().then(address=>{
+      console.log(`${blockchainProvider.extension} -> Address: ${address}`);
 
-      var data = '0x' + new Buffer(message).toString('hex');
-      web3Provider.provider.eth.personal.sign(data, did, "test password!")
+      // actual signing ->>
+      var dataInHex = '0x' + new Buffer(message).toString('hex');
+
+      blockchainProvider.provider.eth.personal.sign(dataInHex, address, "test password!")
       .then(console.log);
+    });
+  
+  }
 
-      // web3.eth.personal.sign(
-      //   web3.fromUtf8(message),
-      //   did,
-      //   (err, signature) => {
-      //     if (err) {
-      //       console.error(`err: ${err}`);
-      //       return;
-      //     }
-      //     console.log(`signature: ${signature}`);
-      //   }
-      // );
+
+  // getIxoDidAsync() {
+  //   return new Promise((resolve, reject)=>{
+  //     resolve(this.blockchainProviders.web3Ixo.provider.debug());
+  //   });
+  // }
+  getIxoDidAsync() {
+    const eth = this.blockchainProviders.web3Ixo.provider.eth;
+    return new Promise((resolve, reject)=>{
+      // resolve(provider.debug());
+      eth.getAccounts(function (error, accounts) {
+        if (error || 0 === accounts.length) {
+          reject(error);
+        }
+        resolve(accounts[0]);
+      });
     });
   }
 
-  // getDidAsyncFromIxoCM() {
-  //   return new Promise((resolve, reject)=>{
-  //     web3Ixo.eth.getAccounts(function (error, accounts) {
-  //       if (error) {
-  //         reject(error);
-  //       }
-  //       resolve(accounts[0]);
-  //     });
-  //   });
-  // }
-
-  getDidAsyncFromProvider(provider) {
+  getEthereumAddressAsync() {
+    const eth = this.blockchainProviders.web3Metamask.provider.eth;
     return new Promise((resolve, reject)=>{
-      provider.eth.getAccounts(function (error, accounts) {
+      // resolve(provider.debug());
+      eth.getAccounts(function (error, accounts) {
         if (error || 0 === accounts.length) {
           reject(error);
         }
@@ -107,15 +113,15 @@ export default class Dashboard extends React.Component {
     return (
       <div>
         <input value={this.state.messageBody} onChange={this.handleMessageBodyChanged} />
-        {web3Providers.web3Metamask.doShow && 
+        {this.blockchainProviders.web3Metamask.doShow && 
           <Launchbutton
-            provider={web3Providers.web3Metamask.id}
+            provider={this.blockchainProviders.web3Metamask.id}
             title="Metamask" 
             handleLaunchEvent={this.handleExtensionLaunch}/>
         }
-        {web3Providers.web3Ixo.doShow && 
+        {this.blockchainProviders.web3Ixo.doShow && 
           <Launchbutton
-            provider={web3Providers.web3Ixo.id}
+            provider={this.blockchainProviders.web3Ixo.id}
             title="IXO CM" 
             handleLaunchEvent={this.handleExtensionLaunch}/>          
         }
